61ac2884953755af76471efbf91d1f1434c81fb1
Dan Patterson <starved14@hotmail.com>
2019-10-07 14:08:29 -0600

    all completed assignments

---
+++ exercises/grocerystore/app.js
@@ -0,0 +1,16 @@
+var shopper = {
+    name: "John",
+    age: 25,
+    single: True,
+
+smiths.grocerystore = function () {
+        return "welcome to smiths"
+    },
+
+grocerycart = ["milk", "eggs", "bread", "steak"]
+
+}
+
+
+
+
