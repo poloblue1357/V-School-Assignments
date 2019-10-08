34bd918c9913d70e2f9a055107893a5091c65777
Dan Patterson <starved14@hotmail.com>
2019-10-07 18:31:43 -0600

    completed array methods and uptown function

---
+++ exercises/uptownfunction/app.js
@@ -0,0 +1,51 @@
+var lyrics = ["This", "hit", "that", "ice", "cold", 
+              "Michelle", "Pfeiffer", "that", "white", 
+              "gold", "This", "one", "for", "them", 
+              "hood", "girls", "Them", "good", "girls", 
+              "straight", "masterpieces", "Stylin'", 
+              "whilen'", "livin'", "it", "up", "in", 
+              "the", "city", "Got", "Chucks", "on", 
+              "with", "Saint", "Laurent", "Gotta", "kiss", 
+              "myself", "I'm", "so", "pretty"];
+// lyrics.toString()
+// console.log(lyrics.join(" ") 
+
+// console.log(lyrics.reverse())
+
+// for (i = 0; i < lyrics.length; i++) {
+//     if ( i % 2 === 0) {
+//         console.log(lyrics[i])
+//     } 
+// }
+
+// for (i = 0; i < lyrics.length; i++) {
+//     if (i % 2 === 0) {
+//         console.log(lyrics.reverse())
+//     } else {
+//         console.log(lyrics[i])
+//     }
+// }
+// console.log(lyrics[i])
+
+// get a random #
+// push variable(word) to new Array
+// remove variable from Array
+// optional: make new array a String
+
+// const newArray = []
+// function randomMath() {
+//     return Math.floor(Math.random() * lyrics.length + 1)
+// }
+// function getRandomLyric() {
+//     for(i = 0; i < lyrics.length; i++) {
+//     let num = randomMath()
+//         newArray.push(lyrics[num])
+//         lyrics.splice(num, 1)
+       
+//     } 
+// }
+// console.log(lyrics.length)
+
+// getRandomLyric()
+//  console.log(newArray)
+//  console.log(newArray.length)
