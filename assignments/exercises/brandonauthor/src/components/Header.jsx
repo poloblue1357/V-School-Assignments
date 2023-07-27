c3292df7da30640d091f9888349be255913bcda1
Dan Patterson <starved14@hotmail.com>
2023-07-27 08:30:34 -0600

    adding picture

--- assignments/exercises/brandonauthor/src/components/Header.jsx
+++ assignments/exercises/brandonauthor/src/components/Header.jsx
@@ -1,9 +1,15 @@
-
+import lastDesi from "./lastDesi.png"
 
 function Header() {
 
     return (
-        <div>Brandon Patterson - Author / Husband / Father</div>
+        <div>
+            Brandon Patterson - Author / Husband / Father
+            <br />
+            <br />
+            <img src={lastDesi} length="400" width="200"/>
+        </div>
+        
     )
 }
 
