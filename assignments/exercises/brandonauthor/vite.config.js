f660cd79a353178857e845f1b3adcdf6545ef2e3
Dan Patterson <starved14@hotmail.com>
2023-08-04 22:44:03 -0600

    tailwind install/setup

--- assignments/exercises/brandonauthor/vite.config.js
+++ assignments/exercises/brandonauthor/vite.config.js
@@ -4,4 +4,30 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:9000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:9000',
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+// });
