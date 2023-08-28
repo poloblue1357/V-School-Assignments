3d892dbd7fc12ca42e042efbf08b978abc3c5f23
Dan Patterson <starved14@hotmail.com>
2023-08-28 16:38:01 -0600

    testing tailwind/updating favicon

--- assignments/exercises/brandonauthor/src/components/Body.jsx
+++ assignments/exercises/brandonauthor/src/components/Body.jsx
@@ -4,11 +4,17 @@ function Body() {
     return(
         <div className="pb-6">
             <div>Header</div>
-            <div className="grid lg:grid-cols-2 gap-4 md:grid-cols-2">
+            <div className="grid lg:grid-cols-2 gap-4 md:grid-cols-2 pb-4">
                 <div className="bg-sky-500 p-6 rounded-lg">1</div>
                 <div className=" bg-red-500 p-6 rounded-lg">2</div>
                 <div className=" bg-red-500 p-6 rounded-lg">3</div>
                 <div className=" bg-red-500 p-6 rounded-lg">4</div>              
+            </div>
+            <div className="inline-block p-4 text-black bg-white border rounded-lg
+             shadow-xl hover:shadow-tBlue">
+                <h1 className="text-2xl">Hello</h1>
+                <p className="mb-4">This is my div</p>
+                <button className="px-3 py-2 rounded-lg cursor-pointer bg-cyan-500 opacity-50">Say Hello</button>
             </div>
         </div>
     )
--- assignments/exercises/brandonauthor/vite.config.js
+++ assignments/exercises/brandonauthor/vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    // vitePluginFaviconsInject('./src/assets/logo.svg')
+  ],
   server: {
     proxy: {
       '/api': {
