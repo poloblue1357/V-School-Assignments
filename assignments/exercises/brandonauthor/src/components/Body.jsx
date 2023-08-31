947de4f12d1a900486e998bf97d83b4010172996
Dan Patterson <starved14@hotmail.com>
2023-08-30 23:16:20 -0600

    testing tailwind

--- assignments/exercises/brandonauthor/src/components/Body.jsx
+++ assignments/exercises/brandonauthor/src/components/Body.jsx
@@ -4,7 +4,7 @@ function Body() {
     return(
         <div className="pb-6">
             <div>Header</div>
-            <div className="grid lg:grid-cols-2 gap-4 md:grid-cols-2 pb-4">
+            <div className="grid lg:grid-cols-2 gap-4 md:grid-cols-2 pb-4 hover:blur-sm">
                 <div className="bg-sky-500 p-6 rounded-lg">1</div>
                 <div className=" bg-red-500 p-6 rounded-lg">2</div>
                 <div className=" bg-red-500 p-6 rounded-lg">3</div>
@@ -15,6 +15,10 @@ function Body() {
                 <h1 className="text-2xl">Hello</h1>
                 <p className="mb-4">This is my div</p>
                 <button className="px-3 py-2 rounded-lg cursor-pointer bg-cyan-500 opacity-50">Say Hello</button>
+            </div>
+            <div className="flex justify-center -space-x-14">
+                <div className="mix-blend-normal rounded-lg bg-blue-400 h-6 w-6"></div>
+                <div className="mix-blend-normal rounded-lg bg-pink-400 h-6 w-6"></div>
             </div>
         </div>
     )
--- assignments/exercises/brandonauthor/src/components/Header.jsx
+++ assignments/exercises/brandonauthor/src/components/Header.jsx
@@ -9,7 +9,11 @@ function Header() {
             <h1 className="hover:font-serif ml-2 text-tBlue">Brandon Patterson - Author / Husband / Father</h1>
             <h1 className="hover:font-serif ml-2 text-tOlive">Brandon Patterson - Author / Husband / Father</h1>
             {/* <div className="flex"> */}
-                <img src={lastDesi} length="400" width="200" className="" />
+                <img src={lastDesi} length="400" width="200" className="
+                grayscale 
+                hover:grayscale-0 
+                hue-rotate-60
+                saturate-200" />
                 <a target="_blank" href="https://www.amazon.com/Client-Brett-Simons-Novel/dp/B0BJY9NLTC">
                     <button className="hover:underline text:md border-2 border-tBlue bg-tDark">
                         Buy Now
