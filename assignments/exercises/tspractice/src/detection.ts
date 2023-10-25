60a320ca896b755b40633341c01c133968ddfbc1
Dan Patterson <starved14@hotmail.com>
2023-10-25 13:32:29 -0600

    fixing ts course

--- assignments/exercises/tspractice/src/detection.ts
+++ assignments/exercises/tspractice/src/detection.ts
@@ -48,3 +48,69 @@ function isAdminAccount(account: User | Admin) {
     }
     
 }
+
+function logValue(x: Date | string) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    } else {
+        console.log(x.toUpperCase());
+    }
+}
+
+type Fish = {swim: () => void}
+type Bird = {fly: () => void}
+
+function isFish(pet: Fish | Bird): pet is Fish {
+    return (pet as Fish).swim !== undefined
+}
+
+function getFood(pet: Fish | Bird) {
+    if(isFish(pet)) {
+        pet
+        return "fish food"
+    } else {
+        pet
+        return "bird food"
+    }
+}
+
+interface Circle {
+    kind: "circle",
+    radius: number
+
+}
+interface Square {
+    kind: "square",
+    side: number
+}
+interface Rectangle {
+    kind: "rectangle", 
+    length: number,
+    width: number
+}
+
+type Shape = Circle | Square | Rectangle
+
+function getTrueShape(shape: Shape) {
+    if(shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2
+    }
+    // return shape.side * shape.side
+}
+
+function getArea(shape: Shape) {
+    switch(shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2
+
+        case "square":
+            return shape.side * shape.side
+
+        case "rectangle": 
+            return shape.length * shape.width
+
+        default:
+            const _defaultForShape: never = shape
+            return _defaultForShape
+    }
+}
