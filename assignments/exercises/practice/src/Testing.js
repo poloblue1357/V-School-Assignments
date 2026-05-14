93981ba981d846b9f6cd5b97bda7724e89f96038
Dan <starved14@hotmail.com>
2026-05-13 23:17:10 -0600

    studying set/map

--- assignments/exercises/practice/src/Testing.js
+++ assignments/exercises/practice/src/Testing.js
@@ -140,15 +140,187 @@
 // }
 // console.log(sequence([1,2,3,10,11,12,13]))
 
-function nearby(nums, k) {
-    const set = new Set()
-    for(const num of nums) {
-        set.has(num)
-    }
+// function nearby(nums, k) {
+//     const set = new Set()
+//     for(const num of nums) {
+//         set.add(num)
+//     }
 
-    for(let i = 0; i < nums.length; i++) {
-        if(set.has(num[i]))
-    }
+//     for(let i = 0; i < k; i++) {
+//         if(nums[0] === nums[i + 1]) {
+//             return true
+//         }
+//     }
     
+// }
+// console.log(nearby([1, 2, 3, 4, 1], 3))
+
+// function nearby(nums, k) {
+//     const set = new Set()
+
+//     for (let i = 0; i < nums.length; i++) {
+//         if (set.has(nums[i])) {
+//             return true
+//         }
+
+//         set.add(nums[i])
+
+//         if (set.size > k) {
+//             set.delete(nums[i - k])
+//         }
+//     }
+
+//     return false
+// }
+
+
+// function unique(s) {
+
+//     const map = new Map()
+//     let str = s.split("")
+
+//     for(const string of s) {
+//         map.set(string, (map.get(string) || 0) + 1)
+//     }
+//     for(let i = 0; i < str.length; i++) {
+//         if(map.get(str[i]) === 1) {
+//             return i
+//         } else {
+//             return -1
+//         }
+//     }
+// }
+// console.log(unique("loveleetcode"))
+
+
+// function anagram(s, t) {
+
+//     const map = new Map()
+//     // let x = s.split("") not necessary
+//     // let y = t.split("") not necessary
+//     if(s.length !== t.length) {
+//         return false
+//     }
+
+//     for(const str of t) {
+//         map.set(str, (map.get(str) || 0) + 1)
+//     }
+//     for(const char of s) {
+//         if(!map.get(char)) {
+//             return false
+//         }
+//         map.set(char, map.get(char) - 1)
+//     }
+//     return true
+// }
+// console.log(anagram('anagram', 'nagaram'))
+
+// function dupes(nums, k) {
+
+//     const set = new Set()
+
+//     for(let i = 0; i < nums.length; i++) {
+
+//         if(set.has(nums[i])) {
+//             return true
+//         }
+
+//         set.add(nums[i])
+
+//         if(set.size > k) {
+//             set.delete(nums[i - k])
+//         }
+//     }
+//     return false
+// }
+// console.log(dupes([1, 2, 3, 1], 3))
+
+// function majority(nums) {
+
+//     const map = new Map()
+
+//     for(const num of nums) {
+//         map.set(num, (map.get(num) || 0) + 1)
+//     }
+
+//     let maxCount = 0
+//     let result = null
+
+//     for(const [num, count] of map) {
+//         if(count > maxCount) {
+//             maxCount = count
+//             result = num
+//         }
+//     }
+//     return result
+// }
+// console.log(majority([3, 2, 3]))
+
+
+// function ransom(s, t) {
+
+//     const map = new Map()
+
+//     for(const str of t) {
+//         map.set(str, (map.get(str) || 0) + 1)
+//     }
+
+//     for(const str of s) {
+//         if(!map.get(str)) {
+//             return false
+//         }
+
+//         map.set(str, map.get(str) - 1)
+//     }
+//     return true
+// }
+// console.log(ransom('ransomNote', 'magazine'))
+
+
+// function unique(s) {
+
+//     const map = new Map()
+
+//     for(const str of s) {
+//         map.set(str, (map.get(str) || 0) + 1)
+//     }
+    
+//     for(let i = 0; i < s.length; i++) {
+//         if(map.get(s[i]) === 1) {
+//             return i
+//         } 
+//     }
+//     return -1
+// }
+// console.log(unique("loveleetcode"))
+
+
+// function ana(s, t) {
+
+//     const map = new Map()
+
+//     for(const str of s) {
+//         map.set(str, (map.get(str) || 0) + 1)
+//     }
+
+//     for(let i = 0; i < t.length; i++) {
+//         if(!map.has(t[i])) {
+//             return false
+//         }
+//         map.set(t[i], map.get(t[i]) - 1)
+//     }
+//     return true
+// }
+// console.log(ana('anagram', 'nagaram'))
+
+
+
+
+function r(s, t) {
+
+    const map = new Map()
+
+    
+
 }
-console.log(nearby([1, 2, 3, 1], 3))
+console.log(r('note', 'magazine'))
